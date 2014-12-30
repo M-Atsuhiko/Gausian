15625b44f8b727b8e9a63522261c465fc99bdc13
M-Atsuhiko <is0042px@ed.ritsumei.ac.jp>
2014-12-30 17:35:09 +0900

    12/30

--- main.sh
+++ main.sh
@@ -37,20 +37,20 @@ fi
 # 実行中
 # excuse time     | server| type    | seed      | dt     | suffix           |
 # ------------------------------------------------------------------------------
-# (12/29 17:30~)  | hal1  | passive | (1:10) | (5:30) | Rerative_75_0       | F:75 C:0 相対的な評価手法をpassiveに試す。理論的な形態の解が本当なら、この方法でも同一の傾向を持つ形になるはず
-# (12/29 17:30~)  | hal2  | ca      | (1:10) | (30)   | Tsuishi_75_0        | F:75 C:0 hal2-hal3-hal4の結果を比較する
-# (12/29 17:35~)  | hal3  | ca      | (1:10) | (30)   | Rerative_liner_75_0 | F:75 C:0
-# (12/29 17:35~)  | hal4  | ca      | (1:10) | (30)   | Rerative_Gaus_75_0  | F:75 C:0
+# ()  | hal1  | passive | () | () |  | F:75 C:0 
+# (12/30 17:30~)  | hal2  | passive | (1:10) | (30)   | Tsuishi_75_0        | F:75 C:0 
+# ()  | hal3  | ca      | () | ()   | | F:0 C:0
+# (12/30 17:30~)  | hal4  | ca      | (1:10) | (30)   | Rerative_Gaus_75_10 | F:75 C:10
 
 
 # (12/17)  | iMac  | passive | ()           | F:75
 
 SEED=(1 2 3 4 5 6 7 8 9 10)
-DELTA_T=(30)
-CONTAIN_CONDUCTANCES="ca"
+DELTA_T=(5 10 15 20 25 30)
+CONTAIN_CONDUCTANCES=""
 Function_Ratio="75"                   #ここでFunction_Ratioを変更できる
 Conductance_Ratio="0"
-Spesific="Rerative_Gaus_"${Function_Ratio}"_"${Conductance_Ratio}     #その他のprefix
+Spesific="Tsuishi_"${Function_Ratio}"_"${Conductance_Ratio}     #その他のprefix
 
 for se in ${SEED[@]}
 do
